374550144e2fdccfff59fb9ff66213e2b4b21db9
William Yataco <will.kidefw@gmail.com>
2023-06-04 15:00:03 -0500

    fix: challenge 11

--- CLASE-04/Challenge/11/11challenge.sh
+++ CLASE-04/Challenge/11/11challenge.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-docker-compose up -d
-sleep 120
-docker exec -it challenge11-web bash
-sudo sed -i 's/172.20.1.101/challenge11-db/g' /var/www/html/index.php
+mkdir lamp-app-ecommerce
+cp -r ../../../CLASE-02/lamp-app-ecommerce/* lamp-app-ecommerce
+docker compose up -d
+sleep 30
+docker ps
+docker exec -t challenge11-php bash -c "sed -i 's/172.20.1.101/mariadb/g' /var/www/html/index.php"
+sleep 30
+docker-compose down -v --rmi all
+rm -r lamp-app-ecommerce
